5e095863a711b0b57db4ddaa7f10e1920f755f93
Alyssa Farina <alyssafarina695@gmail.com>
2019-07-21 00:13:52 -0400

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,21 @@
 class Animal
 
+attr_accessor :weight , :zoo
+attr_reader :nickname , :species
+@@all = [ ]
+
+    def initialize(nickname, weight, species)
+        @nickname = nickname
+        @weight = weight
+        @species = species
+        @@all << self
+end
+
+def self.all
+    @@all
+end
+
+def self.find_by_species(species_type)
+        Animal.all.find {|species| species.animal.species_type == species_type}
+    end
 end
